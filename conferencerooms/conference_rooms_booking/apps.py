75454880cd7c6e45162370a8a370c624e437be22
magdac13 <magda.cebula97@gmail.com>
2023-02-20 21:16:10 +0100

    update 20/02

---
+++ conferencerooms/conference_rooms_booking/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ConferenceRoomsBookingConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "conference_rooms_booking"
